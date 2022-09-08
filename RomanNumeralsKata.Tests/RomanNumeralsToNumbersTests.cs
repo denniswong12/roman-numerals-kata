6afde6980fe1903c632ec9e904a22e8b1aa4e744
Dennis Wong <cfwwong@gmail.com>
2022-09-08 14:21:05 +0100

    Finished first set of test cases for converting Roman Numerals to Numbers

--- RomanNumeralsKata.Tests/RomanNumeralsToNumbersTests.cs
+++ RomanNumeralsKata.Tests/RomanNumeralsToNumbersTests.cs
@@ -2,14 +2,36 @@ namespace RomanNumeralsKata.Tests;
 
 public class RomanNumeralsToNumbersTests
 {
+
+    private RomanNumeralsToNumbers RomanNumeralsToNumbers;
+
     [SetUp]
     public void Setup()
     {
+        RomanNumeralsToNumbers = new RomanNumeralsToNumbers();
     }
 
     [Test]
-    public void Test1()
+    public void Convert_Invalid_Roman_Numerals_Should_Return_Error_Message()
     {
-        Assert.Fail();
+        RomanNumeralsToNumbers.ConvertRomanNum("MEEI").Should().Be("Can't convert \"MEEI\" to Numbers.");
+    }
+
+    [Test]
+    public void Convert_Mixed_Letter_Cases_Should_Return_A_Number()
+    {
+        RomanNumeralsToNumbers.ConvertRomanNum("MDcCViI").Should().Be(1707);
+    }
+
+    [Test]
+    public void Given_Empty_Roman_numeral_Should_Return_Error_Messag()
+    {
+        RomanNumeralsToNumbers.ConvertRomanNum("").Should().Be("Can't convert \"\" to Numbers.");
+    }
+
+    [Test]
+    public void Given_NULL_Roman_numeral_Should_Return_Error_Messag()
+    {
+        RomanNumeralsToNumbers.ConvertRomanNum(null).Should().Be("Can't convert \"\" to Numbers.");
     }
 }
